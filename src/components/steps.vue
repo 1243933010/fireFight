7434063f7f4229658731a5ec8c3ebee82de2aff9
yglbhdy <1181330602@qq.com>
2023-12-01 00:53:45 +0800

    add

--- src/components/steps.vue
+++ src/components/steps.vue
@@ -1,99 +1,165 @@
 <template>
-    <div class="step">
-        <div class="line" v-show="stepList.length>0">
-            <ul class="box">
-                <li class="item" v-for="(item,index) in stepList" :key="index">
-                    <div class="hr" :class="item.isInput?'hr-active':''" v-if="(index+1)!==stepList.length"></div>
-                    <div class="content">
-                        <div class="content-left">
-                            <img v-if="!item.isInput" src="../assets/radio_hui.png" alt="">
-                            <!-- <img src="../assets/radio_lan.png" alt=""> -->
-                            <img v-if="item.isInput" src="../assets/radio_icon.png" alt="">
-                        </div>
-                        <div class="content-right">
-                            <span :class="item.isActive?'active':''">{{ item.title }}</span>
-                            <i class="el-icon-success" v-if="item.isInput&&!item.isActive"></i>
-                        </div>
-                    </div>
-                </li>
-            </ul>
+  <div class="step">
+    <ul class="step-list">
+      <div class="line">
+        <div class="done" :style="`height: ${stepCount * 53}px`"></div>
+        <div
+          class="doing"
+          :style="`height: calc(100% - ${stepCount * 53}px)`"
+        ></div>
+      </div>
+      <li
+        class="step-item"
+        v-for="(item, index) in stepList"
+        :key="index"
+        :class="{ undone: item.isActive, done: item.isInput }"
+      >
+        <div class="space-box" v-if="index !== stepList.length - 1"></div>
+        <div class="step-text">
+          <div class="icon">
+            <img src="../assets/gou.png" class="img" />
+          </div>
+          <div class="text">{{ item.title }}</div>
         </div>
-    </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
-
 <script>
-
 export default {
-    props:{
-        stepList:{
-            type:Array,
-            default:[]
-        }
+  props: {
+    stepList: {
+      type: Array,
+      default: [],
     },
-    data() {
-        return {
-
+  },
+  data() {
+    return {
+      stepH: 53,
+      stepCount: 0,
+      isOver: false,
+    };
+  },
+  mounted() {},
+  watch: {
+    stepList: {
+      handler(newArray) {
+        for (let i = 0; i < newArray.length; i++) {
+          this.stepCount = i;
+          if (!(!newArray[i].isActive && newArray[i].isInput)) {
+            break;
+          }
         }
+      },
+      deep: true,
     },
-    methods: {
-
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.active{
-    color: red;
-}
 .step {
-    width: 100%;
-    background-color: bisque;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  box-sizing: border-box;
+  min-height: 50%;
+  flex-grow: 1;
+
+  .step-list {
+    margin: 0;
+    padding: 0;
+    position: relative;
+
+    .step-item {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      position: relative;
+      z-index: 1;
+
+      .step-text {
+        display: flex;
+        align-items: center;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        .icon {
+          margin-right: 8px;
+          border-radius: 50%;
+          border: 2px solid #a6a9bc;
+          padding: 3px;
+          width: 16px;
+          height: 16px;
+          background-color: #fff;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          img {
+            width: 10px;
+          }
+        }
+
+        .text {
+          font-size: 14px;
+          color: #404659;
+        }
+      }
+
+      &.done {
+        .step-text {
+          .icon {
+            background-color: #3961f8;
+            border: none;
+          }
+        }
+      }
+
+      &.undone {
+        .step-text {
+          .icon {
+            border-color: #3961f8;
+          }
+        }
+      }
+
+      &:last-child {
+        .step-text {
+          position: static;
+        }
+      }
+
+      .space-box {
+        width: 100%;
+        height: 53px;
+      }
+    }
 
     .line {
-        .box {
-            list-style: none;
+      width: 3px;
+      height: 100%;
 
-            .item {
-                position: relative;
-                margin-bottom: 52px;
+      position: absolute;
+      top: 0;
+      left: 6px;
 
-                .hr {
-                    position: absolute;
-                    top: 14px;
-                    left: 6px;
-                    width: 5px;
-                    height: 58px;
-                    background-color: #EAEDEC;
+      .done {
+        background: linear-gradient(0deg, #3958f8 0%, #378cf7 100%);
+        height: 0;
+        transition: height 0.5s;
+      }
 
-                }
-                .hr-active{
-                    background: #3958F8;
-                }
-
-                .content {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    .content-left{
-                        margin-right: 3px;
-                        img{
-                            width: 15px;
-                            height: 15px;
-                        }
-                    }
-                    .content-right{
-                        // width: 120px;
-                        display: flex;
-                        flex-direction: row;
-                        align-self: flex-end;
-                        span{
-                            width: 80px;
-                        }
-                        // float: right;
-                    }
-                }
-            }
-        }
+      .doing {
+        height: 100%;
+        background: #eaedec;
+        transition: height 0.5s;
+      }
     }
-}</style>
+  }
+}
+</style>
--- src/views/projectManagement/add.vue
+++ src/views/projectManagement/add.vue
@@ -1,102 +1,160 @@
 <template>
-    <div style="height: 100%;">
-        <div class="box">
-            <div class="box-left">
-                <div class="steps">
-                    <div class="title">
-                        <img src="../../assets/liucheng.png" alt="" srcset="">
-                        <span>流程指引</span>
-                    </div>
-                    <Steps :stepList="stepList" />
-                </div>
-                <div class="form">
-                    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-                        <el-form-item label="审批人">
-                            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-                        </el-form-item>
-                        <el-form-item label="活动区域">
-                            <el-select v-model="formInline.region" placeholder="活动区域">
-                                <el-option label="区域一" value="shanghai"></el-option>
-                                <el-option label="区域二" value="beijing"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="onSubmit">查询</el-button>
-                        </el-form-item>
-                    </el-form>
-                </div>
+  <div style="height: 100%">
+    <div class="box">
+      <div class="box-left">
+        <div class="steps">
+          <div class="title">
+            <div class="icon">
+              <img src="../../assets/liucheng.png" srcset="" />
             </div>
-            <div class="box-right">
-                <div class="files">
-                    <div class="title">附件</div>
-                </div>
-            </div>
+            <span>流程指引</span>
+          </div>
+          <Steps :stepList="stepList" />
         </div>
+        <div class="form">
+          <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form-item label="审批人">
+              <el-input
+                v-model="formInline.user"
+                placeholder="审批人"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="审批人1">
+              <el-input
+                v-model="formInline.user1"
+                placeholder="审批人1"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="审批人2">
+              <el-input
+                v-model="formInline.user2"
+                placeholder="审批人2"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="审批人3">
+              <el-input
+                v-model="formInline.user3"
+                placeholder="审批人3"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域">
+              <el-select v-model="formInline.region" placeholder="活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <div class="box-right">
+        <div class="files">
+          <div class="title">附件</div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
-
-
 <script>
-import Steps from '@/components/steps.vue'
+import Steps from "@/components/steps.vue";
 export default {
-    components: { Steps },
-    data() {
-        return {
-            formInline: {
-                user: '',
-                region: ''
-            }
-        }
+  components: { Steps },
+  data() {
+    return {
+      formInline: {
+        user: undefined,
+        user1: undefined,
+        user2: undefined,
+        user3: undefined,
+        region: undefined,
+      },
+    };
+  },
+  computed: {
+    stepList() {
+      return [
+        {
+          title: "审批人",
+          isInput: this.formInline.user,
+          isActive: !this.formInline.user && this.formInline.user1,
+        },
+        {
+          title: "审批人1",
+          isInput: this.formInline.user1,
+          isActive: !this.formInline.user1 && this.formInline.user2,
+        },
+        {
+          title: "审批人2",
+          isInput: this.formInline.user2,
+          isActive: !this.formInline.user2 && this.formInline.user3,
+        },
+        {
+          title: "审批人3",
+          isInput: this.formInline.user3,
+          isActive: !this.formInline.user3 && this.formInline.region,
+        },
+        {
+          title: "活动区域",
+          isInput: this.formInline.region,
+          isActive: false,
+        },
+      ];
     },
-    computed: {
-        stepList() {
-            return [
-                {title:'审批人',isInput:this.formInline.user,isActive:!this.formInline.user&&this.formInline.region},
-                {title:'活动区域',isInput:this.formInline.region,isActive:false}
-            ]
-        }
+  },
+  mounted() {},
+  methods: {
+    onSubmit() {
+      console.log("submit!");
     },
-    mounted() {
-
-    },
-    methods: {
-        onSubmit() {
-            console.log('submit!');
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .box {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
+  padding-bottom: 35px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
 
-    .box-left {
-        display: flex;
-        flex-direction: row;
-    }
-}
-.steps{
-    width: 135px;
+  .box-left {
+    display: flex;
     height: 100%;
-    border-right: 1px solid #EAEDEC;
-    title{
+
+    .steps {
+      margin-left: 2px;
+      padding: 20px 16px 0;
+      width: 138px;
+      height: 100%;
+      background-color: #fff;
+
+      display: flex;
+      flex-direction: column;
+
+      .title {
         display: flex;
-        flex-direction: row;
         align-items: center;
         margin-bottom: 42px;
-        img{
-            width: 14px;
-            margin-right: 8px;
+
+        .icon {
+          width: 14px;
+          margin-right: 8px;
+
+          img {
+            width: 100%;
+          }
         }
-        span{
-            color: #404659;
-            font-size: 16px;
+
+        span {
+          color: #404659;
+          font-size: 16px;
         }
+      }
     }
+  }
 }
 </style>
