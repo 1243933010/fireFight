115c951e4d8cb203931b52e308c5b587ee0e9cdf
ysys <1243933010@qq.com>
2024-01-12 22:25:00 +0800

    feat():修复新增项目附件判断问题

--- src/store/modules/projectManagementAdd.js
+++ src/store/modules/projectManagementAdd.js
@@ -26,7 +26,7 @@ const state = {
     {type:'master_check_opinion',title:'重点审查意见书',description:'',files:[],is_required:0},
     {type:'force_check_opinion',title:'驻点审查意见书（支队级）',description:'',files:[],is_required:0},
     {type:'other_opinions',title:'其他意见书',description:'',files:[],is_required:0},
-    {type:'main_party_committee',title:'大队党委会',description:'',files:[],is_required:0},
+    {type:'main_party_committee',title:'大队党委会',description:'',files:[],is_required:1},
     {type:'detachment_party_committee',title:'支队党委会',description:'',files:[],is_required:1},
     {title: "采购计划备案附件", type: "purchase_record",description:'',files:[],is_required:1},
 
