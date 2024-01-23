f25947c9f65b50357afddbdc03295bbd26faf663
ysys <1243933010@qq.com>
2024-01-23 23:42:49 +0800

    feat():修改

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -178,14 +178,20 @@ export default {
       if([1,3].includes(item.status)){
          url = '/projectManagementDetail'
       }
-      if([5,6,7,8,9,10].includes(item.status)){
+      if([5,6,7,8,9,10,11].includes(item.status)){
         url = '/ImplementationCommission/edit'
       }
-      if([11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28].includes(item.status)){
+      if([70,71,72,73,74,75].includes(item.status)){
+        url = '/ImplementationCommission2/edit'
+      }
+      if([76,77,78,79,80,81].includes(item.status)){
+        url = '/ImplementationCommission3/edit'
+      }
+      if([12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28].includes(item.status)){
          url = 'thirdProjects/thirdProjects/edit'
       }
       if([29,30,31,32,33,34,35].includes(item.status)){
-         url = '/contractManagement/edit'
+         url = '/contractManagement/contractManagement/edit'
       }
       this.dialogVisible = false;
       this.$router.push({ path: url,query:{id:item.id} });
